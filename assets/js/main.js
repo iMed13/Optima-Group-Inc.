07e6928d0c85111e465785f4e42a4c40e319454b
imeda <imedabeqaia@gmail.com>
2019-01-10 01:29:21 +0400

    add logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,58 @@
 $(document).ready(function() {
-  $('.preloader').addClass('will-hide');
-  $("html").easeScroll();
-  roundslider('.round-slider-container');
+    $('.preloader').addClass('will-hide');
+    $("html").easeScroll();
+    roundslider('.round-slider-container');
 
-  setTimeout(function() {
-    $("body").removeClass('hide-scroll');
-    $('.preloader').remove();
+    setTimeout(function() {
+        $("body").removeClass('hide-scroll');
+        $('.preloader').remove();
 
-    AOS.init({
-      duration: 1000,
-      once: true
+        AOS.init({
+            duration: 1000,
+            once: true
+        });
+    }, 2000);
+
+    $(document).scroll(function() {
+        if ($(document).scrollTop() > 20 && !$('#top-nav.navbar').hasClass('navbar-is-scrolling')) {
+            $('#top-nav.navbar').addClass('navbar-is-scrolling');
+            $(".navbar-brand img").attr("src", "../assets/images/logowhite.png");
+        } else if ($(document).scrollTop() <= 20 && $('#top-nav.navbar').hasClass('navbar-is-scrolling')) {
+            $('#top-nav.navbar').removeClass('navbar-is-scrolling');
+            $(".navbar-brand img").attr("src", "../assets/images/logo.png");
+        }
+    }).scroll();
+
+    $('#topnavbar').on('show.bs.collapse', function() {
+        $('#top-nav').addClass('collapsed');
+    }).on('hide.bs.collapse', function() {
+        $('#top-nav').removeClass('collapsed');
     });
-  }, 2000);
 
-  $(document).scroll(function() {
-    if ($(document).scrollTop() > 20 && !$('#top-nav.navbar').hasClass('navbar-is-scrolling'))
-      $('#top-nav.navbar').addClass('navbar-is-scrolling');
-    else if ($(document).scrollTop() <= 20 && $('#top-nav.navbar').hasClass('navbar-is-scrolling'))
-      $('#top-nav.navbar').removeClass('navbar-is-scrolling');
-  }).scroll();
+    $('.team-members').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+        if (nextSlide == 0 && currentSlide != 1)
+            roundslider_next('.round-slider-container', 0);
+        else if (currentSlide == 0 && nextSlide != 1)
+            roundslider_prev('.round-slider-container', 0);
+        else if (currentSlide > nextSlide)
+            roundslider_prev('.round-slider-container', 0);
+        else if (currentSlide < nextSlide)
+            roundslider_next('.round-slider-container', 0);
+    });
 
-  $('#topnavbar').on('show.bs.collapse', function() {
-    $('#top-nav').addClass('collapsed');
-  }).on('hide.bs.collapse', function() {
-    $('#top-nav').removeClass('collapsed');
-  });
+    $(document).on('click', 'a[href^="#"]', function(event) {
+        event.preventDefault();
 
-  $('.team-members').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-    if (nextSlide == 0 && currentSlide != 1)
-      roundslider_next('.round-slider-container', 0);
-    else if (currentSlide == 0 && nextSlide != 1)
-      roundslider_prev('.round-slider-container', 0);
-    else if (currentSlide > nextSlide)
-      roundslider_prev('.round-slider-container', 0);
-    else if (currentSlide < nextSlide)
-      roundslider_next('.round-slider-container', 0);
-  });
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - $('#top-nav').outerHeight() - 20
+        }, 1000);
+    });
 
-  $(document).on('click', 'a[href^="#"]', function(event) {
-    event.preventDefault();
-
-    $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top - $('#top-nav').outerHeight() - 20
-    }, 1000);
-  });
-
-  $('.video-popup').magnificPopup({
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-  });
+    $('.video-popup').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+    });
 
 });
