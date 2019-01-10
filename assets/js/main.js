6d93c20b98b3263d03795b788003280dc8bc9b47
imeda <imedabeqaia@gmail.com>
2019-01-10 14:00:49 +0400

    menu change, logo change,last

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,10 +16,10 @@ $(document).ready(function() {
     $(document).scroll(function() {
         if ($(document).scrollTop() > 20 && !$('#top-nav.navbar').hasClass('navbar-is-scrolling')) {
             $('#top-nav.navbar').addClass('navbar-is-scrolling');
-            $(".navbar-brand img").attr("src", "assets/images/logowhite.png");
+            $(".submenu li a").addClass("submenucolor");
         } else if ($(document).scrollTop() <= 20 && $('#top-nav.navbar').hasClass('navbar-is-scrolling')) {
             $('#top-nav.navbar').removeClass('navbar-is-scrolling');
-            $(".navbar-brand img").attr("src", "assets/images/logo.png");
+            $(".submenu li a").removeClass("submenucolor");
         }
     }).scroll();
 
