2fe94850c72f22a3f58af5d9692e0723699042e8
imeda <imedabeqaia@gmail.com>
2019-01-10 15:46:15 +0400

    image change, slider change

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,10 +16,10 @@ $(document).ready(function() {
     $(document).scroll(function() {
         if ($(document).scrollTop() > 20 && !$('#top-nav.navbar').hasClass('navbar-is-scrolling')) {
             $('#top-nav.navbar').addClass('navbar-is-scrolling');
-            $(".submenu").addClass("submenuactive");
+            $(".submenu li a").addClass("submenuactive");
         } else if ($(document).scrollTop() <= 20 && $('#top-nav.navbar').hasClass('navbar-is-scrolling')) {
             $('#top-nav.navbar').removeClass('navbar-is-scrolling');
-            $(".submenu").removeClass("submenuactive");
+            $(".submenu li a").removeClass("submenuactive");
         }
     }).scroll();
 
